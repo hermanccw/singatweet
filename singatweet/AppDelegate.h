890de477872fb891b74f8009824f9cd2f0cf3886
herman <hermancc@gmail.com>
2015-01-15 22:03:13 -0500

    Initial Commit

---
+++ singatweet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  singatweet
+//
+//  Created by Herman Chan on 2015-01-15.
+//  Copyright (c) 2015 ironark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ singatweet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  singatweet
+//
+//  Created by Herman Chan on 2015-01-15.
+//  Copyright (c) 2015 ironark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
